af7e4727a4e35792fb8a2e90abe124984da1df45
Vaclav Barta <vbarta@mangrove.cz>
2022-03-11 11:04:12 +0100

    fixed test of hooks-slot-type-limit

--- clang-tools-extra/test/clang-tidy/checkers/hooks-slot-type-limit.c
+++ clang-tools-extra/test/clang-tidy/checkers/hooks-slot-type-limit.c
@@ -7,7 +7,7 @@ extern int64_t slot_type(uint32_t slot, uint32_t flags);
 int64_t hook(int64_t reserved)
 {
     int64_t amt_slot = 0, is_xrp = slot_type(amt_slot, 2);
-// CHECK-MESSAGES: :[[@LINE-1]]:46: warning: function slot_type accepts only 0 or 1 flags [hooks-slot-type-limit]
+// CHECK-MESSAGES: :[[@LINE-1]]:56: warning: function slot_type accepts only 0 or 1 flags [hooks-slot-type-limit]
 
     slot_type(0, 0);
 // CHECK-MESSAGES: :[[@LINE-1]]:15: warning: function slot_type may not access more than 255 slots [hooks-slot-type-limit]
